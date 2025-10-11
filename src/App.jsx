9d83d04ec45fc2f8ed2294749b4bd71cd6956158
Kharybin Ruslan <kharyb@bk.ru>
2025-10-11 17:06:53 +0300

    Props and hooks

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import PopNewCard from "./components/popups/PopNewCard/PopNewCard";
+import PopBrowse from "./components/popups/PopBrowse/PopBrowse";
+import PopUser from "./components/popups/PopUser/PopUser";
+
+function App() {
+  return (
+    <div className="wrapper">
+      {/* pop-up start*/}
+      <PopUser />
+      <PopNewCard />
+      <PopBrowse />
+      {/* pop-up end*/}
+
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
